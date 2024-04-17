e4ac74e01e61877cd7eca4c3e892ad661e29a102
awezmehtab <shaikarifa178@gmail.com>
2024-04-17 17:22:40 +0530

    able to generate genre-based ratings

--- web/app.js
+++ web/app.js
@@ -22,6 +22,7 @@ fs.readFile('imdb.json', 'utf8', (err, data) => {
         const imdb_data = JSON.parse(data);
         imdb_data.forEach((m, index, array) => {
             // movieData contains arrays in string form, let's parse it first then push it to movies array
+            m.Genres = m.Genres.replace(/'/g, '\"');
             movieData.push(m);
         });
     }
@@ -54,23 +55,23 @@ app.post('/submit', (req, res) => {
         try {
             const imdb_data = JSON.parse(data);
             // movieData contains arrays in string form, let's parse it first then push it to movies array
-            const movieData = imdb_data.find((m, index, array) => movieEntered.toLowerCase() === m.Title.toLowerCase());
+            const movieDataFound = imdb_data.find((m, index, array) => movieEntered.toLowerCase() === m.Title.toLowerCase());
 
             // no need to check if something's wrong, i kept it in ejs file
-            if(movieData) {
-                movieData.Casts = movieData.Casts.replace(/'/g, '\"');
-                movieData.Casts = JSON.parse(movieData.Casts);
-                movieData.Directors = movieData.Directors.replace(/'/g, '\"');
-                movieData.Directors = JSON.parse(movieData.Directors);
-                movieData.Other_Ratings = movieData.Other_Ratings.replace(/'/g, '\"');
-                movieData.Other_Ratings = JSON.parse(movieData.Other_Ratings);
-                movieData.Genres = movieData.Genres.replace(/'/g, '\"');
-                movieData.Genres = JSON.parse(movieData.Genres);
-                movieData.Streaming_Platforms = movieData.Streaming_Platforms.replace(/'/g, '\"');
-                movieData.Streaming_Platforms = JSON.parse(movieData.Streaming_Platforms);
+            if(movieDataFound) {
+                movieDataFound.Casts = movieDataFound.Casts.replace(/'/g, '\"');
+                movieDataFound.Casts = JSON.parse(movieDataFound.Casts);
+                movieDataFound.Directors = movieDataFound.Directors.replace(/'/g, '\"');
+                movieDataFound.Directors = JSON.parse(movieDataFound.Directors);
+                movieDataFound.Other_Ratings = movieDataFound.Other_Ratings.replace(/'/g, '\"');
+                movieDataFound.Other_Ratings = JSON.parse(movieDataFound.Other_Ratings);
+                movieDataFound.Genres = movieDataFound.Genres.replace(/'/g, '\"');
+                movieDataFound.Genres = JSON.parse(movieDataFound.Genres);
+                movieDataFound.Streaming_Platforms = movieDataFound.Streaming_Platforms.replace(/'/g, '\"');
+                movieDataFound.Streaming_Platforms = JSON.parse(movieDataFound.Streaming_Platforms);
             }
 
-            movies.push(movieData);
+            movies.push(movieDataFound);
             res.redirect('/'); 
         }
         catch(err) {
@@ -80,10 +81,14 @@ app.post('/submit', (req, res) => {
 });
 
 ratedMovies = []
+suggestedMovies = []
 ratings = []
+
 
 // GET request for /ratings
 app.get('/ratings', (req, res) => {
+    ratedMovies = [];
+    ratings = [];
     warning = 0;
     res.render('ratings', { ratedMovies: ratedMovies, ratings: ratings, warning: warning});
 });
@@ -97,6 +102,7 @@ app.post('/ratings', (req, res) => {
     if(movie) {
         if (rating > 0 && rating < 10) {
             movie.Genres = movie.Genres.replace(/'/g, '\"');
+            movie.Genres = JSON.parse(movie.Genres);
             const index = ratedMovies.findIndex(ratedMovie => ratedMovie.Title.toLowerCase() === movie.Title.toLowerCase());
             if(index !== -1) {
                 ratings[index] = rating;
@@ -111,6 +117,50 @@ app.post('/ratings', (req, res) => {
     else {
         warning = 1; // 1 represents movie not found
     };
+
+
+    // Suggesting Movies!
+
+    genresLiked = []
+    genreTotalRating = {}
+    genreRating = {}
+    genreNumbers = {}
+    
+    // gathering all genres liked by user
+    ratedMovies.forEach((movie, index, array) => {
+        movie.Genres.forEach((genre, index, array) => {
+            if(genresLiked.indexOf(genre) === -1) {
+                genresLiked.push(genre);
+            }
+        });
+    });
+
+    // number of movies corresponding to each genre, minimum will be 1 ofcourse
+    ratedMovies.forEach((movie, index, array) => {
+        movie.Genres.forEach((genre, index, array) => {
+            if(genreNumbers[genre] === undefined) {
+                genreNumbers[genre] = 1;
+            } else {
+                genreNumbers[genre]++;
+            }
+        });
+    });
+
+    // overall rating of each genre which is sum of ratings in each movie, which is then divided by number of movies corresponding to that genre
+    ratedMovies.forEach((movie, i, array) => {
+        movie.Genres.forEach((genre, j, array) => {
+            if(genreTotalRating[genre] === undefined) {
+                genreTotalRating[genre] = Number(ratings[i]);
+            } else {
+                genreTotalRating[genre] += Number(ratings[i]);
+            }
+        });
+    });
+
+    // dividing by number of movies corresponding to that genre
+    genresLiked.forEach((genre, index, array) => {
+        genreRating[genre] = Number(genreTotalRating[genre])/Number(genreNumbers[genre]);
+    });
 
     res.render('ratings', { ratedMovies: ratedMovies, ratings: ratings, warning: warning });
 });
