5ce3770fd48db8e0be9a1ffaad8f9780cb295822
awezmehtab <shaikarifa178@gmail.com>
2024-04-17 16:06:29 +0530

    can give reviews now

--- web/app.js
+++ web/app.js
@@ -5,11 +5,29 @@ const bodyParser = require('body-parser');
 
 // creating an express "app"
 const app = express();
-const port = 3001;
+const port = 3002;
 
 // all the movies used by this server.
 // this contains an array of all movies, each movie is unstringified
 const movies = [];
+const movieData = []
+
+// reading imdb.json
+fs.readFile('imdb.json', 'utf8', (err, data) => {
+    if(err) {
+        console.log('error reading imdb.json ' + err);
+    }
+    try {
+        const imdb_data = JSON.parse(data);
+        imdb_data.forEach((m, index, array) => {
+            // movieData contains arrays in string form, let's parse it first then push it to movies array
+            movieData.push(m);
+        });
+    }
+    catch(err) {
+        console.log('Sorry, something\'s wrong in parsing imdb.json :', err);
+    }
+});
 
 // setting view engine
 app.set('view engine', 'ejs');
@@ -60,13 +78,49 @@ app.post('/submit', (req, res) => {
     });
 });
 
+ratedMovies = []
+ratings = []
+
+// GET request for /ratings
+app.get('/ratings', (req, res) => {
+    warning = 0;
+    res.render('ratings', { ratedMovies: ratedMovies, ratings: ratings, warning: warning});
+});
+
+// POST request for /ratings
+app.post('/ratings', (req, res) => {
+    const rating = req.body.rating;
+    const movieName = req.body.movieName;
+    const movie = movieData.find((m, index, array) => movieName.toLowerCase() === m.Title.toLowerCase());
+    warning = 0;
+    if(movie) {
+        if (rating > 0 && rating < 10) {
+            movie.Genres = movie.Genres.replace(/'/g, '\"');
+            const index = ratedMovies.findIndex(ratedMovie => ratedMovie.Title.toLowerCase() === movie.Title.toLowerCase());
+            if(index !== -1) {
+                ratings[index] = rating;
+            } else {
+                ratedMovies.push(movie);
+                ratings.push(rating);
+            }
+        } else {
+            warning = 2; // 2 represents invalid rating
+        }
+    }
+    else {
+        warning = 1; // 1 represents movie not found
+    };
+
+    res.render('ratings', { ratedMovies: ratedMovies, ratings: ratings, warning: warning });
+})
+
 // my middleware to handle error
 app.use((err, req, res, next) => {
     console.log(err);
     res.status(500).send('kuch GANDA sa GALAT hua hai: ' + err);
 });
 
-// all requests taken from port 3001
+// all requests taken from port 3002
 app.listen(port, () => {
     console.log(`port ${port} ki request sun rahe hai bas`)
 })
