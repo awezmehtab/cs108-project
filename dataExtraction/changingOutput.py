1212db5c6f5f37dc397b4799daaa14387c188326
awezmehtab <shaikarifa178@gmail.com>
2024-04-27 00:45:46 +0530

    added more reviews by directly searching in rt, not in flight now

--- dataExtraction/changingOutput.py
+++ dataExtraction/changingOutput.py
@@ -28,14 +28,16 @@ def click(driver, webElement):
         wait = WebDriverWait(driver, MAX_WAIT_UNTIL_BUTTON_LOADS)
         button = wait.until(EC.element_to_be_clickable(webElement))
         button.click()
+        return
     except:
         try:
             # let's try to scroll and check
             button = webElement
-            driver.execute_script("arguments[0].scrollIntoView(false);", button)
+            driver.execute_script("arguments[0].scrollIntoView(true);", button)
             button.click()
         except Exception as e:
             print(e, file=open('errors.txt', 'a'))
+        return
 
 for movieReviewed in reviews:
     if not movieReviewed['RottenTomatoes']:
@@ -45,49 +47,54 @@ for movieReviewed in reviews:
                 try:
                     import ast
                     directors = ast.literal_eval(movie['Directors'])
+                    casts = ast.literal_eval(movie['Casts'])
                     break
                 except Exception as e:
                     print(movie['Directors'], type(movie['Directors']))
                     print(e)
                 break
-        search_str = f'{movie["Title"]} {directors[0]}'.strip().replace(' ', '%20').replace(':', '%3A').replace(',', '%2C').replace("'", '%27')
+        directors_str = " ".join(directors)
+        casts_str = " ".join(casts)
+        search_str = f'{movie["Title"]} {directors_str} {casts_str}'.strip().replace(' ', '%20').replace(':', '%3A').replace(',', '%2C').replace("'", '%27')
         search_url = f'https://www.rottentomatoes.com/search?search={search_str}'
 
-         # let's use driver to get info now
-        driver.get(search_url)
+        try:
+            # let's use driver to get info now
+            driver.get(search_url)
 
-        # list of webelements which contain the clickable movie name (link)
-        searchedMovie = driver.find_elements(By.CSS_SELECTOR, 'a[data-qa="info-name"]')[0]
+            # list of webelements which contain the clickable movie name (link)
+            searchedMovie = driver.find_elements(By.CSS_SELECTOR, 'a[data-qa="info-name"]')[0]
 
-        # let's click it using our click() function
-        click(driver, searchedMovie)
+            # let's click it using our click() function
+            click(driver, searchedMovie)
 
-        # so we're into the MOVIE INFORMATION PAGE now
+            # so we're into the MOVIE INFORMATION PAGE now
 
-        # our task is to click the critcs reviews link, we'll try to get the corresponding WebElement first
-        reviewsButton = driver.find_element(By.CSS_SELECTOR, 'div#critics-consensus a')
-        click(driver, reviewsButton)
+            # our task is to click the critcs reviews link, we'll try to get the corresponding WebElement first
+            reviewsButton = driver.find_element(By.CSS_SELECTOR, 'div#critics-consensus a')
+            click(driver, reviewsButton)
 
-        topCriticsButton = driver.find_element(By.CSS_SELECTOR, 'a.tabs__link.js-tab-link[data-type="top-critics"]')
-        click(driver, topCriticsButton)
+            topCriticsButton = driver.find_element(By.CSS_SELECTOR, 'a.tabs__link.js-tab-link[data-type="top-critics"]')
+            click(driver, topCriticsButton)
 
-        movieReviewed["RottenTomatoes"] = []
+            movieReviewed["RottenTomatoes"] = []
 
-        reviewsWebElements = driver.find_elements(By.CSS_SELECTOR, 'div.review-row')
-        print("mil gaye itne reviews: ", len(reviews))
-        for review in reviewsWebElements:
-            reviewSel = Selector(text=review.get_attribute('innerHTML'))
-            personName = reviewSel.css('a.display-name::text').get()
-            personLink = reviewSel.css('a.display-name::attr(href)').get()
-            reviewText = reviewSel.css('p.review-text::text').get()
-            try:
-                movieReviewed["RottenTomatoes"].append([personName.strip(), f'https://rottentomatoes.com/{personLink.strip()}', reviewText])
-            except Exception as e:
-                movieReviewed["RottenTomatoes"].append(["", "", reviewText])
-                print(e)
-        
-        with open('dynamic_output.json', 'w') as f:
-            json.dump(reviews, f)
+            reviewsWebElements = driver.find_elements(By.CSS_SELECTOR, 'div.review-row')
+            print(f'{movieReviewed["Title"]} ke itne reviews:', len(reviews))
+            for review in reviewsWebElements:
+                reviewSel = Selector(text=review.get_attribute('innerHTML'))
+                personName = reviewSel.css('a.display-name::text').get()
+                personLink = reviewSel.css('a.display-name::attr(href)').get()
+                reviewText = reviewSel.css('p.review-text::text').get()
+                try:
+                    movieReviewed["RottenTomatoes"].append([personName.strip(), f'https://rottentomatoes.com/{personLink.strip()}', reviewText])
+                except Exception as e:
+                    movieReviewed["RottenTomatoes"].append(["", "", reviewText])
+                    print(e)
+            
+                with open('output.json', 'w') as f:
+                    json.dump(reviews, f)
 
-with open('output.json', 'w') as f:
-    json.dump(reviews, f)
+        except Exception as e:
+            print(f"An error occurred: {e}")
+            print(e, file=open('errors.txt', 'a'))
