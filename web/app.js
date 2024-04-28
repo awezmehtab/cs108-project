422c76d945d3dc584d6fe5a2835ae0b5874a1aa6
awezmehtab <shaikarifa178@gmail.com>
2024-04-28 12:24:42 +0530

    Website looks good overall, FINALISATION 1

--- web/app.js
+++ web/app.js
@@ -7,7 +7,7 @@ const { distance, closest } = require('fastest-levenshtein');
 
 // creating an express "app"
 const app = express();
-const port = 3020;
+const port = 3030;
 
 // all the movies used by this server.
 // this contains an array of all movies, each movie is unstringified
